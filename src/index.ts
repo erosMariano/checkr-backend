4cff3c56e9ca8dd13e070a0d26a813daebf88159
erosMariano <erosmariano1@gmail.com>
2023-11-16 20:48:43 -0300

    deploy vercel

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,10 @@ import routes from "./routes";
 
 AppDataSource.initialize().then(async () => {
   const app = express();
-
+  
+  app.use(cors());
   app.use(bodyParser.json());
 
-  app.use(cors());
-
   app.use(routes);
-  app.listen(3333);
+  app.listen(process.env.PORT || 3333);
 });
